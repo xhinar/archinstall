eb06a6e617f7d975190019496e37bf8b0a00fb31
xhinar <31232568+xhinar@users.noreply.github.com>
2018-06-13 15:36:08 +0700

    Update setupdisk.sh
    * 01. create_partitions
    * 02. format fat32
    * 03. format btrfs
    * 04. mount chroot
    * 05. create key
    * 06. set as swap

--- setupdisk.sh
+++ setupdisk.sh
@@ -7,7 +7,9 @@ INS_EFI_PART="${INS_DISK}1"
 INS_SWAP_PART="${INS_DISK}2"
 INS_SLASH_PART="${INS_DISK}3"
 
-### begin - create_partitions ###
+INS_PASSWORD="4557UK1035ZN"
+
+### begin - 01. create_partitions ###
 sgdisk \
   --clear \
   --new 1::"$INS_EFI_SIZE" \
@@ -19,9 +21,38 @@ sgdisk \
   --new 3:: \
       --change-name 3:LUKS_SLASH \
       --typecode 3:8300 \
-  "INS_DISK"
-### create_partitions - end ###
+  "$INS_DISK"
+### 01. create_partitions - end ###
   
-### begin - format fat 32 ###
+### begin - 02. format fat32 ###
 mkfs.fat -F32 "$INS_EFI_PART"
-### format fat 32 - end ###
+### 02. format fat32 - end ###
+
+INS_SLASH_CONTAINER="$(cryptsetup_slash "$INS_PASSWORD" "$INS_SLASH_PART")"
+
+### begin - 03. format btrfs ###
+mkfs.btrfs --label SLASH "/dev/mapper/$INS_SLASH_CONTAINER"
+### 03. format btrfs - end ###
+
+### begin - 04. mount chroot ###
+mount "/dev/mapper/$INS_SLASH_CONTAINER" /mnt
+mkdir /mnt/boot
+mount "$INS_EFI_PART" /mnt/boot
+### 04. mount chroot - end ###
+
+### begin - 05. create key ###
+SWAP_KEY="/mnt$INS_SWAP_KEY"
+mkdir -p "$(dirname $SWAP_KEY)"
+dd bs=512 count=1 if=/dev/random of="$SWAP_KEY" status=none
+### 05. create key - end ###
+
+INS_SWAP_CONTAINER="$(cryptsetup_swap "$INS_SWAP_PART" "/mnt$INS_SWAP_KEY")"
+
+### begin - 06. set as swap ###
+mkswap --label SWAP "/dev/mapper/$INS_SWAP_CONTAINER"
+swapon "/dev/mapper/$INS_SWAP_CONTAINER"
+### 06. set as swap - end ###
+
+
+
+    
